4032256b95ad68295703121eeb95f626252a85fd
nadisan <nadiawellela@gmail.com>
2024-02-27 18:17:32 +0300

    tsd

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...myArgs) {
-  let argcount = 0 ;
-  for (const arg of myArgs) {
-    argcount += 1;
-  }
-  return argcount;
+  return myArgs.length;
 }
