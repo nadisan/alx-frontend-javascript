cc0c09afe1f2086a72b9348dcedd53ffe09d47d6
nadisan <nadiawellela@gmail.com>
2024-03-11 17:46:05 +0300

    task3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -20,16 +20,16 @@ export default class Currency {
 
   set name(name) {
     if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = name;
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
   }
 
   set code(code) {
     if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      this._code = code;
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
   }
 
   displayFullCurrency() {
