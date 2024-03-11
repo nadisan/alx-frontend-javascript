d711b8382de3a379422898c91ae266a56def18d1
nadisan <nadiawellela@gmail.com>
2024-03-11 11:35:19 +0300

    redotask10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  //eslint-disable-next-line
+  // eslint-disable-next-line
   for (const idx of array) {
-    //eslint-disable-next-line
-    array[array.indexof(value)] = appendString + value;
+    // eslint-disable-next-line
+    array[array.indexof(idx)] = appendString + idx;
   }
   return array;
-  }
-  
+}
