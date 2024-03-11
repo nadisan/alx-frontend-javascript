7cb01577757ceda5d7b33d317729b316c45db3f0
nadisan <nadiawellela@gmail.com>
2024-03-11 16:45:37 +0300

    task2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string'){
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
@@ -11,12 +11,15 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+  
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
